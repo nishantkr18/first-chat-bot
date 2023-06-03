65ec5db2dfc14c039a5b44e0051c97724a891e7e
nishantkr18 <nniishantkumar@gmail.com>
2023-06-03 21:18:39 +0530

    Added better exception handling

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
+"""
+Generates output based on user input using OpenAI's Davinci engine.
+"""
+
+import os
 import openai
 import streamlit as st
-import os
+from openai import error
 
 # To use .env file
 from dotenv import load_dotenv
@@ -10,15 +15,16 @@ openai.api_key = os.getenv("API_KEY")
 
 st.title("Ask me anything!")
 
-input = st.text_input("Enter your question here", key="prompt")
+text_input = st.text_input(
+    '', key="prompt", placeholder="Enter your question here", label_visibility="collapsed")
 
 # Checks if input is empty
-if st.session_state.prompt:
+if text_input != '':
     with st.spinner("Thinking..."):
         try:
             response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=f"Q: {st.session_state.prompt}\nA:",
+                engine="text-davinci-001",
+                prompt=f"Q: {text_input}\nA:",
                 temperature=0.9,
                 max_tokens=100,
                 top_p=1,
@@ -28,5 +34,5 @@ if st.session_state.prompt:
             st.write(response.choices[0].text)
             with st.expander("Full Response"):
                 st.write(response)
-        except Exception as e:
-            st.write("error: ", e)
+        except error.OpenAIError as e:
+            st.write("An issue with OpenAI has occured:", e)
